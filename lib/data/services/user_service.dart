65e45c6b1a8c3dfd0497fc103d341d2003aed8a8
Tanakorn Khunkhao <hopeza081081081@gmail.com>
2023-10-26 01:33:09 +0700

    refactor

--- lib/data/services/user_service.dart
+++ lib/data/services/user_service.dart
@@ -1,4 +1,5 @@
 import 'package:dtacexamtest/data/repositories/user_repository.dart';
+import 'package:dtacexamtest/models/user/user_model.dart';
 import 'package:dtacexamtest/models/user/user_view_model.dart';
 
 abstract class UserService {
@@ -12,8 +13,33 @@ class UserServiceImpl implements UserService {
   @override
   Future<List<UserViewModel>> getUsersService(String amount) async {
     try {
-      List<UserViewModel> listOfUserViewModel =
+      UsersModel userResult =
           await userRepository.getUsersRepository(amount);
+
+      List<UserViewModel> listOfUserViewModel = [];
+      listOfUserViewModel.addAll(
+        userResult.results!.map(
+          (userResultsElement) {
+            String name =
+                '${userResultsElement.name!.title} ${userResultsElement.name!.first} ${userResultsElement.name!.last}';
+            String gender = userResultsElement.gender.toString();
+            String email = userResultsElement.email.toString();
+            String age = userResultsElement.dob!.age.toString();
+            String mobileNumber = userResultsElement.phone.toString();
+            String imageUrl = userResultsElement.picture!.medium.toString();
+
+            return UserViewModel(
+              name: name,
+              gender: gender,
+              email: email,
+              age: age,
+              mobileNumber: mobileNumber,
+              imageUrl: imageUrl,
+            );
+          },
+        ),
+      );
+
       return listOfUserViewModel;
     } catch (_) {
       rethrow;
